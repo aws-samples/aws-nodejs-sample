e5484c93f8ebe6d88be4d26bf9bcc27f1c172c9a
Viki <vigneshvrn42@gmail.com>
2022-07-18 14:54:19 +0530

    Updated SDK version and fixed code and package.json for uuid

--- sample.js
+++ sample.js
@@ -1,36 +1,37 @@
 /*
  * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-**/
+ **/
 
 // Load the SDK and UUID
-var AWS = require('aws-sdk');
-var uuid = require('node-uuid');
+var AWS = require("aws-sdk");
+var uuid = require("uuid");
 
 // Create an S3 client
 var s3 = new AWS.S3();
 
 // Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
+var bucketName = "node-sdk-sample-" + uuid.v4();
+var keyName = "hello_world.txt";
 
-s3.createBucket({Bucket: bucketName}, function() {
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-  s3.putObject(params, function(err, data) {
-    if (err)
-      console.log(err)
+s3.createBucket({ Bucket: bucketName }, function () {
+  var params = { Bucket: bucketName, Key: keyName, Body: "Hello World!" };
+  s3.putObject(params, function (err, data) {
+    if (err) console.log(err);
     else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+      console.log(
+        "Successfully uploaded data to " + bucketName + "/" + keyName
+      );
   });
 });
